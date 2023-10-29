545b58bd339f34d133f521743c1c5d4c0ec33fbb
Aleksandr Bolotov <projectfebr@gmail.com>
2023-10-29 20:02:29 +0300

    создан редюсер

---
+++ ComposableArchitectureSample/CounterFeature.swift
@@ -0,0 +1,30 @@
+//
+//  CounterFeature.swift
+//  ComposableArchitectureSample
+//
+//  Created by Александр Болотов on 29.10.2023.
+//
+
+import ComposableArchitecture
+
+struct CounterFeature: Reducer {
+    struct State {
+        var count = 0
+    }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+
+    func reduce(into state: inout State, action: Action) -> ComposableArchitecture.Effect<Action> {
+        switch action {
+        case .decrementButtonTapped:
+            state.count -= 1
+            return .none
+        case .incrementButtonTapped:
+            state.count += 1
+            return .none
+        }
+    }
+}
