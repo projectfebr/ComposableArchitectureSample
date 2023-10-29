8955d2d6fbc0934cc0dbc4034cdcfc5a5e3be84a
Aleksandr Bolotov <projectfebr@gmail.com>
2023-10-29 19:37:04 +0300

    Initial Commit

---
+++ ComposableArchitectureSample/ComposableArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureSampleApp.swift
+//  ComposableArchitectureSample
+//
+//  Created by Александр Болотов on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
